5b93b4e89a2525e1b069a0f65ac1837fc4427340
Kanchan <kanchan08642@gmail.com>
2023-01-23 18:18:09 +0530

    commit

--- components/src/Components/Firstpage/HomePage.js
+++ components/src/Components/Firstpage/HomePage.js
@@ -1,147 +1,84 @@
-import React from 'react'
-import "./HomePage.css"
- import img1 from "../../assets/img1.png"
-// import background from "./assets/background.jpg"
-// import intelLogo from "./assets/intelLogo.avif"
-// import amazoneLogo from "./assets/amazonLogo.avif"
-// import behanceLogo from "./assets/behanceLogo.png"
-// import facebookLogo from "./assets/facebookLogo.png"
-// import slackLogo from "./assets/slackLogo.png"
-// import ibmLogo from ".//assets/ibmLogo.avif"
-// import hexagone from "./assets/hexagone.avif"
+import React from "react";
+import "./index.css";
+// import amazoneLogo from "../../assets/amazonLogo.avif"
+
+import intelLogo from "../../assets/intelLogo.avif"
+import amazoneLogo from "../../assets/amazonLogo.avif"
+import behanceLogo from "../../assets/behanceLogo.avif"
+import facebookLogo from "../../assets/facebookLogo.avif"
+import slackLogo from "../../assets/slackLogo.avif"
+import ibmLogo from "../../assets/ibmLogo.avif"
+// import hexagone from "../../assets/hexagone.avif"
 
 const FirstPage = () => {
   return (
-    
+    <div className="main-caintainer ">
+      <div className="container ">
+        <div className="row">
+          <div className="col-4">
+          <div className="hexagon pt-5">beehee hive</div>
+          </div>
+          <div className="col-8">
 
-
-    <div className='container mt-2'>
-          <div className='row'>
-            <div className='col-4'>
-              <div id='hexagon1'>
-
+            <div className="box-rigth pt-5">
+            <div className="row">
+              <div className="col-4">
+                <h2>100%</h2>
+                <p>SaaS technology</p>
               </div>
-              ghdjgkjlk
-            {/* <div className="hexagon"
-               id="hexagon">
-            </div> */}
+              <div className="col-4">
+                 <h2>100+</h2>
+               <p>Threat intel sources</p>
+              </div>
+              <div className="col-4">
+                <h2>100+</h2>
+               <p>Threat intel sources</p>
+              </div>
             </div>
-            <div className='col-8'>
-              <div className='row'>
-                <div className='col-4'>
-                <h2>100%</h2>
-             <p>SaaS technology</p>
-                  </div> 
-                <div className='col-4'>
-                <h2>100%</h2>
-             <p>SaaS technology</p>
-                  </div> 
-                <div className='col-4'>
-                <h2>100%</h2>
-             <p>SaaS technology</p>
-                  </div> 
-              </div>
 
-              <div className='row'>
-                <div className='col-4'>
+            <div className="row mt-5">
+              <div className="col-4">
                 <h2>100%</h2>
-             <p>SaaS technology</p>
-                  </div> 
-                <div className='col-4'>
-                <h2>100%</h2>
-             <p>SaaS technology</p>
-                  </div> 
-                <div className='col-4'>
-                <h2>100%</h2>
-             <p>SaaS technology</p>
-                  </div> 
+                <p>SaaS technology</p>
+              </div>
+              <div className="col-4">
+                <h2>1,400+</h2>
+          <p>Buit in queries</p>
+              </div>
+              <div className="col-4">
+                 <h2>200+</h2>
+                 <p>Costomer</p>
               </div>
             </div>
           </div>
-
+        </div>
+        </div>
+        <div className="footer-box mt-5">
+        <div className="row">
+          <div className="col-2">
+            <img src={intelLogo}/>
+          </div>
+          <div className="col-2">
+            <img src={amazoneLogo}/>
+          </div>
+          <div className="col-2">
+            <img src={behanceLogo}/>
+          </div>
+          <div className="col-2">
+            <img src={facebookLogo}/>
+          </div>
+          <div className="col-2">
+            <img src={slackLogo}/>
+          </div>
+          <div className="col-2">
+            <img src={ibmLogo}/>
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
 
-
-
-
-     
-    // <div className='Main'>
-    //   {/* <div className='background' style={{backgroundImage:`url(${background})`, backgroundSize:`cover`,backgroundRepeat:`no-repeat`}}>  */}
-    //    {/* <img src={background}/> */}
-    //   <div className='top'>
-    //     <div className='image'>
-    //       <img src={img1}/>
-    //     </div>
-    //     {/* <div className='
-    //     '> */}
-    //       <div className='top-content'>
-    //         <div className='must-content'>
-    //           <h2>100%</h2>
-    //           <p>SaaS technology</p>
-    //           </div>
-    //           <div className='must-content'>
-    //            <h2>100,000</h2>
-    //           <p>Endpoint per tenant</p>
-    //           </div>
-    //           <div className='must-content'> 
-    //           <h2>100+</h2>
-    //           <p>Threat intel sources</p>
-    //           </div>
-    //         </div>
-    //         <div className='bottom-content'>
-    //           <div className='must-content'>
-    //           <h2>124</h2>
-    //           <p>Action project</p>
-                
-    //           </div>
-    //           <div className='must-content'>
-    //           <h2>1,400+</h2>
-    //           <p>Buit in queries</p>
-    //           </div>
-    //           <div className='must-content'>
-    //           <h2>200+</h2>
-    //           <p>Costomer</p>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-
-
-/* 
-        <div className='logo'>
-          <div>
-          <img className='logo-svg' src={intelLogo}/>
-          </div>
-          <div>
-          <img className='logo-svg' src={amazoneLogo}/>
-          </div>
-          <div>
-          <img  className='logo-svg'src={behanceLogo}/>
-          </div>
-          <div>
-          <img className='logo-svg' src={facebookLogo}/>
-          </div>
-          <div>
-          <img className='logo-svg' src={slackLogo}/>
-          </div>
-          <div>
-          <img className='logo-svg' src={ibmLogo}/>
-          </div>
-         
-        </div> */
-    // 
-      // </div>
-
-
-
-   
-
-
-
-
-    
-    
   );
-}
+};
 
-export default FirstPage
+export default FirstPage;
