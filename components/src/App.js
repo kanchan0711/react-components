edc65683e4d7aec25de4cde1611eb54f8dfcc61f
Kanchan <kanchan08642@gmail.com>
2023-01-25 13:11:56 +0530

    commit

--- components/src/App.js
+++ components/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
-import HomePage from './Components/Firstpage/HomePage';
+import HomePage from './Components/Page/HomePage/HomePage';
+import SupportFeature from './Components/SupportFeature/SupportFeature';
+import Footer from './Components/Page/Footer/Footer';
 
 function App() {
   return (
+    <>
    <HomePage/>
+   <SupportFeature/>
+   <Footer/>
+   </>
   );
 }
 
--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App /> 
   </React.StrictMode>
 );
 
+
+
+
+
+
+
+
+
+    // <div className="main-caintainer ">
+    //   <div className="container ">
+    //     <div className="row">
+    //       <div className="col-4">
+    //       <div className="hexagon pt-5">beehee hive</div>
+    //       </div>
+    //       <div className="col-8">
+
+    //         <div className="box-rigth pt-5">
+    //         <div className="row">
+    //           <div className="col-4">
+    //             <h2>100%</h2>
+    //             <p>SaaS technology</p>
+    //           </div>
+    //           <div className="col-4">
+    //              <h2>100+</h2>
+    //            <p>Threat intel sources</p>
+    //           </div>
+    //           <div className="col-4">
+    //             <h2>100+</h2>
+    //            <p>Threat intel sources</p>
+    //           </div>
+    //         </div>
+
+    //         <div className="row mt-5">
+    //           <div className="col-4">
+    //             <h2>100%</h2>
+    //             <p>SaaS technology</p>
+    //           </div>
+    //           <div className="col-4">
+    //             <h2>1,400+</h2>
+    //       <p>Buit in queries</p>
+    //           </div>
+    //           <div className="col-4">
+    //              <h2>200+</h2>
+    //              <p>Costomer</p>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     </div>
+    //     <div className="footer-box mt-5">
+    //     <div className="row">
+    //       <div className="col-2">
+    //        logo       </div>
+    //       <div className="col-2">
+    //        logo         </div>
+    //       <div className="col-2">
+    //        logo         </div>
+    //       <div className="col-2">
+    //        logo          </div>
+    //       <div className="col-2">
+    //        logo       </div>
+    //       <div className="col-2">
+    //        logo     </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
